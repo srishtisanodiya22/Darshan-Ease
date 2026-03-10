9a38a8620aea64dacab6e68a1dbbf06096998101
Srishti Sanodiya <srishtisanodiya2004@gmail.com>
2026-03-10 22:22:44 +0530

    fix1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
-    port: 5173
+    port: 5173,
   }
 });
-
